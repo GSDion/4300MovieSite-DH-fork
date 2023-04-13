9fd13d38d9789b9c8e8ef7db26813a29fd35a7da
ccchitwood <ccc14685@uga.edu>
2023-04-12 21:27:33 -0400

    Login boxes/CSS for logged in/logged out
    Functionality for logging in and adding movies still in progress.

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,8 @@ function Header() {
         <img src={process.env.PUBLIC_URL + '/logo.jpg'} alt="logo" />
       </div>
       <h1 className="header__title">DCC Movie Reviews</h1>
-      <button id="addMovieButton">Add Movie</button>
+      <h2 className="welcome">Welcome *username*!</h2> 
+      <button className="addmoviebutton">Add Movie</button>
     </header>
   );
 } else {
@@ -22,6 +23,13 @@ function Header() {
         <img src={process.env.PUBLIC_URL + '/logo.jpg'} alt="logo" />
       </div>
       <h1 className="header__title">DCC Movie Reviews</h1>
+      <div className="loginsection"> 
+        <label for="username">Username</label>
+        <input type="text" placeholder="Enter Username" name="username" required></input>
+        <label for="password">Password</label>
+        <input type="text" placeholder="Enter Password" name="password" required></input>
+        <button id="loginbutton">Login</button>
+      </div>
     </header>
   );
 }
