09c965966f03faea241b7ec0a8bd1d8e46d3e24a
ccchitwood <ccc14685@uga.edu>
2023-04-14 22:42:24 -0400

    Functioning home button on add items page

--- src/components/secondheader.js
+++ src/components/secondheader.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './secondheader.css';
 
+import { Link } from 'react-router-dom';
+
 function SecHeader() {
     return (
     <header className="secheader">
       <div className="secheader__homebutton">
+        <Link to="/">
         <img src={process.env.PUBLIC_URL + '/homebutton.png'} alt="homebutton" />
+        </Link>
       </div>
       <h1 className="secheader__title">DCC Movie Reviews</h1>
     </header>
