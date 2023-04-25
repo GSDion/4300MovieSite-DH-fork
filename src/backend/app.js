e02adf318f81f1693df4c4eda054497bf4fc18fe
GSDion <97761993+GSDion@users.noreply.github.com>
2023-04-24 23:32:03 -0400

    Update app.js
    > fix front-end to back-end connection error.
    > fixed cors header error

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello world!'));
-app.get('/', (req, res) => res.send('Hello World!'));
+
+// Port 8082 for back-end server, Port 3000 for front-end
 const port = process.env.PORT || 8082;
+
+//cors HAS to be before any app.get calls
+const cors = require('cors');
+app.use(cors({ origin: true, credentials: true}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+  });
+
+// Displayed on browser
+app.get('/', (req, res) => res.send('Hello world!'));
+
 //app.listen(port, () => console.log(`Server running on port
 //${port}`));
 const users = require("./routes/api/users");
 app.use('/api/users', users);
-
 const mongoose = require('mongoose');
-const cors = require('cors');
 const items = require('./routes/api/items');
-app. use('/api/items',items);
-// Connect Database
-app.use(cors({ origin: true, credentials: true}));
+app.use('/api/items',items);
 
+// Connect to Database
 app.use(express.json({extended: false}));
-
-//app.get('/', (req, res) => res.send('Hello World!'));
-
 const conn_str = 'mongodb+srv://dh67149:mI0UNYi19IlRaHMo@cluster0.o8nso8o.mongodb.net/?retryWrites=true&w=majority';
-
 mongoose.set('strictQuery', false);
 mongoose.connect(conn_str, {
     useUnifiedTopology : true,
@@ -33,3 +40,4 @@ mongoose.connect(conn_str, {
 .catch(err => {
     console.log(`Erro in DB Connection ${err}`);
 })
+
